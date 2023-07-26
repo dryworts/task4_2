c64a4a00fb701bb6b8add4ba545542e1b6f552eb
dryworts <omzg@mail.ru>
2023-07-26 23:23:18 +0600

    reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public int calculate(double weight, double height){
-        return (int) (weight/(height*height));
+    public int calculate(double weight, double height) {
+        return (int) (weight / (height * height));
     }
 }
